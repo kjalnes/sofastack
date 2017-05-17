e7f7944f0b9dbfa30d43fa3b15979abb138209ae
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-05-16 20:53:14 -0400

    added constants

---
+++ shared/attrTypes.js
@@ -0,0 +1,13 @@
+const DATE = 'DATE';
+const STRING = 'STRING';
+const INTEGER = 'INTEGER';
+const BOOLEAN = 'BOOLEAN';
+
+const types = {
+  DATE,
+  STRING,
+  INTEGER,
+  BOOLEAN
+};
+
+module.exports = Object.assign({}, types, {default: types});
