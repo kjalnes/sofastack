ac8359edbd102e26bc29f11b63dc471ea2c68ebb
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-06-01 21:47:35 -0400

    added make repo

---
+++ server/github/makeRepo.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+module.exports = (accessKey, name) => new Promise((reject, resolve) => {
+
+  request({
+    method: 'POST',
+    uri: 'https://api.github.com/user/repos?access_token=' + accessKey,
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/vnd.github.v3+json',
+      'User-Agent': 'sofa stack'
+    },
+    body: JSON.stringify({
+      name
+    })
+  }, (err, gitResponse, body) => {
+    if (err){
+      return reject(err);
+    }
+    return resolve(body);
+  });
+
+});
