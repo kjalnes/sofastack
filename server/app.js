567550a886363585407b3bbbc3bf85d84beb0282
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-06-01 21:36:30 -0400

    added github auth

--- server/app.js
+++ server/app.js
@@ -3,6 +3,50 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+
+const session = require('express-session');
+
+app.use(session({
+  secret: 'kitty',
+  name: 'summer',
+  resave: false,
+  saveUninitialized: true
+}));
+
+//set up passport
+
+const passport = require('passport');
+var GitHubStrategy = require('passport-github').Strategy;
+
+const gitConfig = process.env.GITHUB_URL ? {
+  clientID: process.env.GITHUB_ID,
+  clientSecret: process.env.GITHUB_SECRET,
+  callbackURL: process.env.GITHUB_URL
+} : require('../config');
+
+passport.use(new GitHubStrategy(gitConfig,
+  function(accessToken, refreshToken, profile, cb) {
+    cb(null, accessToken);
+  }
+));
+
+
+app.use(passport.initialize());
+
+app.get('/auth/github',
+  passport.authenticate('github', {scope: ['repo', 'repo_deployment', 'delete_repo']}));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/',
+    session: false
+  }),
+  function(req, res) {
+    req.session.keys = req.session.keys || {};
+    req.session.keys.github = req.user;
+    res.redirect('/');
+  });
+
 app.use(bodyParser.json({limit: '50mb'}));
 
 app.use('/vendor', express.static(path.join(__dirname, '..', 'node_modules')));
