e22b353c1432686c6e115981534452374121492d
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-06-10 17:34:49 -0400

    added body code

--- shared/build/File/index.js
+++ shared/build/File/index.js
@@ -1,7 +1,7 @@
 const File = function({headers, body, footer}){
-  this.headers = headers || [];
-  this.body = body || null;
-  this.footer = footer || null;
+  this.headers = headers || {};
+  this.body = body || {};
+  this.footer = footer || {};
 };
 
 File.prototype.getHeader = function(){
@@ -19,18 +19,31 @@ File.prototype.getHeader = function(){
   return header.join('\n');
 };
 
+File.prototype.getBody = function(){
+  const body = [];
+  let bodyStrs = this.body;
+  for (let key in bodyStrs){
+    if (typeof bodyStrs[key] === 'string'){
+      body.push(bodyStrs[key]);
+    } else {
+      body.push(bodyStrs[key]());
+    }
+  }
+  return body.join('\n');
+};
+
 File.prototype.toString = function(){
   let file = this.getHeader();
-  if (this.body && this.headers.length > 0){
+  if (Object.keys(this.body).length && Object.keys(this.headers).length){
     file = file + '\n\n';
   }
-  file = this.body ? file + this.body : file;
+  file = this.body ? file + this.getBody() : file;
 
-  if (this.body && this.footer){
-    file = file + '\n\n';
-  }
+  // if (Object.keys(this.body).length && Object.keys(this.footer).length){
+  //   file = file + '\n\n';
+  // }
 
-  file = this.footer ? file + this.footer : file;
+  // file = this.footer ? file + this.footer : file;
   return file;
 };
 
