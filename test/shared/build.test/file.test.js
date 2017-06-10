ad91f2182dece2dab706501107b281c3d56ffb1e
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-06-10 17:06:33 -0400

    another test case

--- test/shared/build.test/file.test.js
+++ test/shared/build.test/file.test.js
@@ -3,11 +3,18 @@ import {expect} from 'chai';
 
 describe('File', () => {
 
-  it.only('can make headers', () => {
+  it('can make headers', () => {
     const headers = {test: 'test', Test: 'test2'};
     const file = new File({headers});
     expect(file.getHeader()).to.eql(`const test = require('test');
 const Test = require('test2');`);
   });
 
+
+  it('can make file of only headers', () => {
+    const headers = {test: 'test', Test: 'test2'};
+    const file = new File({headers});
+    expect(file.toString()).to.eql(`const test = require('test');
+const Test = require('test2');`);
+  });
 });
