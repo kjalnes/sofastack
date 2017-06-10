4c1994f660bbe1537a79e140d1b4af320b7a0ddd
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-06-10 16:54:38 -0400

    added header functions

---
+++ shared/build/File/index.js
@@ -0,0 +1,33 @@
+const File = function({headers, body, footer}){
+  this.headers = headers || [];
+  this.body = body || null;
+  this.footer = footer || null;
+};
+
+File.prototype.getHeader = function(){
+  const header = [];
+  let headers = this.headers;
+  for (let key in headers){
+    if (!Array.isArray(headers[key])){
+      header.push(`const ${key} = require(${headers[key]})`);
+    } else {
+      header.push(`require(${headers[key]})`);
+    }
+  }
+  return header.join('\n');
+};
+
+File.prototype.toString = function(){
+  let file = this.getHeader();
+  if (this.body && this.headers.length > 0){
+    file = file + '\n\n';
+  }
+  file = this.body ? file + this.body : file;
+
+  if (this.body && this.footer){
+    file = file + '\n\n';
+  }
+
+  file = this.footer ? file + this.footer : file;
+  return file;
+};
