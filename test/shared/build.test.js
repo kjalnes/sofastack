4d261774020c462c6e81597f326ba6e73d62edf2
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-06-10 16:18:24 -0400

    updated text

--- test/shared/build.test.js
+++ test/shared/build.test.js
@@ -14,10 +14,11 @@ describe('Build Code', () => {
       const folder = new Folder();
       folder.add('/test/index.js', 'abc');
       expect(folder.test).to.be.exist;
+      expect(folder.test instanceof Folder).to.be.true;
       expect(folder.test['index.js']).to.eql('abc');
     });
 
-    it.only('can add 2 folder and add a file', () => {
+    it('can add 2 folders and add a file', () => {
       const folder = new Folder();
       folder.add('/test/test2/index.js', 'abc');
       expect(folder.test).to.be.exist;
